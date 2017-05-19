ef1d2e1a9c9b2bfe59694e1658cfd993ce09fab6
michael lara garcia <osita0713_@live.com.ar>
2017-05-19 00:10:43 -0500

    Agrego clase Laptop

---
+++ Proyecto/Proyecto/laptop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto
+{
+    class Laptop
+    {
+        string ubicacion;
+        int id = 0;
+        double Potencia;
+        public Laptop(double Potencia, string ubicacion)
+        {
+            this.Potencia = Potencia;
+            this.ubicacion = ubicacion;
+            id += 1;
+        }
+    }
+}
