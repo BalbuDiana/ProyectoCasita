33d6f5e7a15d0ff64ebf93ffcbe30ab9d19851b4
ErikJaimez <reik1923@gmail.com>
2017-05-23 16:46:29 -0500

    Las clases creadas deben de tener herencia

---
+++ Proyecto/Proyecto/Clase_prueba.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto
+{
+    class Clase_prueba
+    {
+
+        //Se necesita utilizar herencia para los electrodomesticos 
+    }
+}
