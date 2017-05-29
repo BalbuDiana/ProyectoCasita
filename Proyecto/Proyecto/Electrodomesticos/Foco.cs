5be007f07ffc82648d9778a11a7e19a5f75ca4cd
Diana Balbuena <dianabalbuenavalencia@outlook.com>
2017-05-29 17:57:24 -0500

    Se añade Foco y lampara :3

--- Proyecto/Proyecto/Electrodomesticos/Foco.cs
+++ Proyecto/Proyecto/Electrodomesticos/Foco.cs
@@ -6,58 +6,14 @@ using System.Threading.Tasks;
 
 namespace Proyecto.Electrodomesticos
 {
-    class Foco:Electro_master
+    class Foco: Electro_master
     {
-        DateTime tiempoIni;		// En fecha
-        double tiempoAcc;       // En horas 
-        double Potencia;
+        public Foco(double potencia, string id, string ubicacion)
+            :base(potencia, id, ubicacion)
 
-        public Foco(double Potencia, string id, string ubicacion)
-            :base(Potencia,id, ubicacion)
         {
-            this.id = id;
-            this.ubicacion = ubicacion;
-            this.estaPrendido = false;
-            this.Potencia = Potencia;
-            this.potencia = Potencia;
-            tiempoAcc = 0.0;
+
         }
 
-        public override double Consumosuma()  //para saber el consumo de estar prendido 
-        {
-            // SI el aparato esta actualmente prendido, se suma "temporalmente el tiempo que lleva encendido. 
-            double tiempoAux = tiempoAcc;
-            if (estaPrendido)
-            {
-                tiempoAux = tiempoAux + (DateTime.Now.Subtract(tiempoIni).TotalHours);
-            }
-
-            return Potencia * tiempoAux; //me da el consumo de Wh
-        }
-
-
-        public override void Apagar() //para cambiar estado
-        {
-            if (estaPrendido)
-            {
-                estaPrendido = false;
-                DateTime tiempoFin = DateTime.Now; //es local porque no se utiliza en otro lugar
-
-                // Al operar DateTime (sumar, restart, etc ),  nos devuelve una objeto
-                // de tipo TimeSpan, el cual representa un intervalo de tiempo (no una fecha)
-                // Este intervalos lo podemos obtener en horas la variable TotalHours.
-                tiempoAcc = tiempoAcc + (tiempoFin.Subtract(tiempoIni).TotalHours);
-            }
-        }
-
-
-        public override void Prender() //para cambiar el estado inicial 
-        {
-            if (!estaPrendido)
-            {
-                estaPrendido = true;
-                tiempoIni = DateTime.Now; //guardo el tiempo 
-            }
-        }
     }
 }
