10790bb879de727d7131d96c2acc95c0f3bd7a76
Diana Balbuena <dianabalbuenavalencia@outlook.com>
2017-05-29 16:36:29 -0500

    Cambié errores que se tendrán al impleamentar mis clases

--- Proyecto/Proyecto/Electrodomesticos/Consola.cs
+++ Proyecto/Proyecto/Electrodomesticos/Consola.cs
@@ -14,5 +14,20 @@ namespace Proyecto.Electrodomesticos
         {
 
         }
+
+        public override void Apagar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double Consumosuma()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Prender()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
