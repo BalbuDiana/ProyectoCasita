fc6d4f4668ead2c1df888f5715e3f95307e92832
Diana Balbuena <dianabalbuenavalencia@outlook.com>
2017-05-29 16:23:12 -0500

    Agregué mis clases :3

---
+++ Proyecto/Proyecto/Electrodomesticos/Consola.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Electrodomesticos
+{
+    class Consola: Electro_master
+
+    {
+        public Consola(double potencia, string id, string ubicacion)
+            :base(potencia, id, ubicacion)
+        {
+
+        }
+    }
+}
